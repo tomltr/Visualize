d87a45921ce3ae9bbed5ece85eced4e6111cd940
Tom <tomletrn@gmail.com>
2020-02-19 09:54:08 -0800

    Add build to project

---
+++ build/precache-manifest.d9fe371b1bed61a8ec2e31b5465acc48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159862ea248dd03d9d8967ff06da34c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe3ee5176b678162732",
+    "url": "/static/css/main.23454100.chunk.css"
+  },
+  {
+    "revision": "85414a4bb5df7ca45b58",
+    "url": "/static/js/2.154e3469.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.154e3469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe3ee5176b678162732",
+    "url": "/static/js/main.f038195e.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
