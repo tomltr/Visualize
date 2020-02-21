9085b1badb5139af3b1e0944df9660f0f30be48c
Tom <49661849+tomltr@users.noreply.github.com>
2020-02-21 08:50:38 -0800

    Removing get_login from routes.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const users_controller = require('../controllers/users');
 
 router.post('/register', users_controller.post_register_user);
-router.get('/login', users_controller.get_login);
 router.post('/login', users_controller.post_login);
 
 module.exports = router;
